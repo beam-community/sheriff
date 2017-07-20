0d5b9301bc746a34482881be36f850b4fb6d93ca
Sean Callan <seancallan@gmail.com>
2017-07-20 11:17:41 -0600

    Correct issue with helpers not using configured key

--- lib/sheriff/law_enforcer.ex
+++ lib/sheriff/law_enforcer.ex
@@ -5,8 +5,6 @@ defmodule Sheriff.LawEnforcer do
 
   import Sheriff.Plug
 
-  alias Sheriff.Plug
-
   @doc false
   def init(opts), do: opts
 
@@ -14,27 +12,18 @@ defmodule Sheriff.LawEnforcer do
   Apply a law against the current actor and requested resource.
   """
   def call(conn, opts) do
-    law = Keyword.fetch!(opts, :law)
+    action   = conn_action(conn)
+    actor    = current_actor(conn)
+    law      = Keyword.fetch!(opts, :law)
+    resource = current_resource(conn)
 
-    conn
-    |> fetch_actor(opts)
-    |> fetch_resource
-    |> legal?(law)
+    if legal?(law, action, actor, resource) do
+      conn
+    else
+      handle_error(:unauthorized, conn, opts)
+    end
   end
 
-  defp fetch_actor(conn, opts) do
-    resource_key = Plug.from_opts(opts, :resource_key, :current_user)
-    {conn.private[resource_key], conn, opts}
-  end
-
-  defp fetch_resource({actor, conn, opts}) do
-    resource = Plug.current_resource(conn)
-    {actor, resource, conn, opts}
-  end
-
-  defp legal?({actor, resource, conn, opts}, law) do
-    permitted = apply(law, :legal?, [actor, conn_action(conn), resource])
-    if permitted, do: conn, else: handle_error(:unauthorized, conn, opts)
-  end
-  defp legal?(conn, _), do: conn
+  defp legal?(law, action, actor, resource),
+    do: apply(law, :legal?, [actor, action, resource])
 end
--- lib/sheriff/plug.ex
+++ lib/sheriff/plug.ex
@@ -4,21 +4,21 @@ defmodule Sheriff.Plug do
   helper functions.
   """
 
-  import Plug.Conn, only: [halt: 1]
+  import Plug.Conn, only: [halt: 1, put_private: 3]
 
   @doc """
   Retrieve the current Actor resource.
   """
 
   @spec current_actor(Plug.Conn.t) :: any
-  def current_actor(conn), do: conn.private[:sheriff_actor]
+  def current_actor(conn), do: conn.private[actor_key()]
 
   @doc """
   Retrieve the current resource for the request.
   """
 
   @spec current_resource(Plug.Conn.t) :: any
-  def current_resource(conn), do: conn.private[:sheriff_resource]
+  def current_resource(conn), do: conn.private[resource_key()]
 
   @spec conn_action(Plug.Conn.t) :: atom | {atom, binary}
   def conn_action(conn), do: conn.private[:phoenix_action] || conn_tuple(conn)
@@ -36,6 +36,12 @@ defmodule Sheriff.Plug do
     |> halt
   end
 
+  def set_current_actor(conn, actor), do: put_private(conn, actor_key(), actor)
+
+  def set_current_resource(conn, resource), do: put_private(conn, resource_key(), resource)
+
+  defp actor_key, do: Application.get_env(:sheriff, :actor_key, :sheriff_actor)
+
   defp conn_tuple(conn) do
     method =
       conn.method
@@ -44,4 +50,6 @@ defmodule Sheriff.Plug do
 
     {method, conn.request_path}
   end
+
+  defp resource_key, do: Application.get_env(:sheriff, :resource_key, :sheriff_resource)
 end
